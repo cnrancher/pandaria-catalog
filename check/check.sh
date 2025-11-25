1250f1b18a75da53b47a99ad55fbde3e3c7cb752
StarryWang <starry.wang@suse.com>
2025-11-25 17:13:07 +0800

    Update for 2.13

--- check/check.sh
+++ check/check.sh
@@ -8,6 +8,12 @@ RANCHER_VERSION="${RANCHER_VERSION:-}"
 BRANCH_NAME=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
 if [[ -z "$RANCHER_VERSION" ]]; then
     case $BRANCH_NAME in
+        */v2.14)
+            RANCHER_VERSION="v2.14"
+            ;;
+        */v2.13)
+            RANCHER_VERSION="v2.13"
+            ;;
         */v2.12)
             RANCHER_VERSION="v2.12"
             ;;
@@ -31,8 +37,8 @@ if [[ -z "$RANCHER_VERSION" ]]; then
             ;;
         *)
             echo "Could not get Rancher version from git branch [$BRANCH_NAME]"
-            echo "Set Rancher version to v2.12"
-            RANCHER_VERSION="v2.12"
+            echo "Set Rancher version to v2.13"
+            RANCHER_VERSION="v2.13"
             ;;
     esac
 fi
